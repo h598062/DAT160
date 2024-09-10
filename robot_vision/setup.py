6dd9f4d4c0b81e0950a158473efc57bed7ca2a99
h598062 <598062@stud.hvl.no>
2024-09-10 12:30:23 +0200

    add robot vision package

---
+++ robot_vision/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'robot_vision'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rocotics',
+    maintainer_email='598062@stud.hvl.no',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
