f0c13a143354c1307a024e97bc61699d8f091335
h598062 <598062@stud.hvl.no>
2024-09-10 13:52:06 +0200

    start assignment 2

--- robot_vision/robot_vision/tb3_blob_tracker.py
+++ robot_vision/robot_vision/tb3_blob_tracker.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python3
 
-import rclpy
-from rclpy.node import Node
 import cv2
 import numpy as np
+import rclpy
 from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+
 
 class BlobTracker(Node):
     def __init__(self):
-        super().__init__('blob_tracker_node')
+        super().__init__("blob_tracker_node")
         self.bridge = CvBridge()
-        self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
-        
-        #TODO: Make a subscriber to camera topic '/camera/image_raw' with message type 'Image'
-        #and callback function self.image_callback
+        self.cmd_vel_pub = self.create_publisher(Twist, "/cmd_vel", 10)
 
+        # TODO: Make a subscriber to camera topic '/camera/image_raw' with message type 'Image'
+        # and callback function self.image_callback
+        self.cam_sub = self.create_subscription(
+            Image, "/camera/image_raw", self.image_callback, 10
+        )
 
-        #TODO: Create SimpleBlobDetector_Params object and define filter parameters
+        # TODO: Create SimpleBlobDetector_Params object and define filter parameters
+        params = cv2.SimpleBlobDetector_Params()
 
-        
-        #TODO: Create a SimpleBlobDetector object with the parameter object defined above and store it as a property of the class
+        # Filter by Area.
+        params.filterByArea = False
+        params.minArea = 15
+        params.maxArea = 6000
 
-        
+        # Filter by Circularity
+        params.filterByCircularity = False
+        params.minCircularity = 0.1
+
+        # Filter by Convexity
+        params.filterByConvexity = False
+        params.minConvexity = 0.5
+
+        # Filter by Inertia
+        params.filterByInertia = False
+        params.minInertiaRatio = 0.01
+
+        # TODO: Create a SimpleBlobDetector object with the parameter object defined above and store it as a property of the class
+        self.detector = cv2.SimpleBlobDetector_create(params)
+
     def image_callback(self, data):
         # This converts the image from ROS Image message to OpenCV format
         try:
@@ -31,16 +51,14 @@ class BlobTracker(Node):
         except CvBridgeError as e:
             self.get_logger().error(f"CvBridge Error: {e}")
             return
-        
 
-        #TODO: Implement blob detection on the camera image 'cv_image'.
-        #Consider changing color space, thresholding and the filter parameters for the blob detection.
-        #Remember that based on your thresholding method, you might have to invert the binary image with the ~ operator before passing it to the detector.
+        # TODO: Implement blob detection on the camera image 'cv_image'.
+        # Consider changing color space, thresholding and the filter parameters for the blob detection.
+        # Remember that based on your thresholding method, you might have to invert the binary image with the ~ operator before passing it to the detector.
         #
-        #Return the result of the blob detection into the variable 'keypoints':
-        #keypoints = 
+        # Return the result of the blob detection into the variable 'keypoints':
+        # keypoints =
 
-        
         if keypoints:
             # Assuming the biggest blob is the desired one
             # You can change this logic to track a different blob, but choosing the biggest one worked fine for me
@@ -51,16 +69,21 @@ class BlobTracker(Node):
             self.visual_servoing(keypoint_to_track, cv_image)
 
             # Draw detected blobs
-            blobs = cv2.drawKeypoints(cv_image, [keypoint_to_track], np.zeros((1, 1)),
-                                      (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            blobs = cv2.drawKeypoints(
+                cv_image,
+                [keypoint_to_track],
+                np.zeros((1, 1)),
+                (0, 255, 0),
+                cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
+            )
         else:
             # No blobs found, stop the robot
             twist = Twist()
             self.cmd_vel_pub.publish(twist)
-            
+
             # Only display original image if no blobs found
             blobs = cv_image
-        
+
         cv2.imshow("Blob Tracker", blobs)
         cv2.waitKey(1)
 
@@ -68,7 +91,7 @@ class BlobTracker(Node):
         # DO NOT MODIFY THIS FUNCTION
         # Visual servoing logic to control the robot
         x, y = keypoint.pt
-        blob_size = keypoint.size        
+        blob_size = keypoint.size
         twist = Twist()
         # Calculate the linear velocity based on the size of the blob
         desired_size = 80  # desired size
@@ -78,8 +101,10 @@ class BlobTracker(Node):
         # Calculate angular velocity to keep the blob centered horizontally
         error_x = x - (cv_image.shape[1] / 2)
         twist.angular.z = -error_x / 650.0  # Proportional control
-        twist.angular.z = np.clip(twist.angular.z, -1.5, 1.5)  # Limit the angular speed        
+        # Limit the angular speed
+        twist.angular.z = np.clip(twist.angular.z, -1.5, 1.5)
         self.cmd_vel_pub.publish(twist)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -90,5 +115,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- robot_vision/setup.py
+++ robot_vision/setup.py
@@ -1,25 +1,40 @@
 from setuptools import setup
+import os
+from glob import glob
 
-package_name = 'robot_vision'
+package_name = "robot_vision"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*.launch.py")),
+        ),
+        (
+            os.path.join("share", package_name, "worlds"),
+            glob(os.path.join("worlds", "*.world")),
+        ),
+        (
+            os.path.join("share", package_name, "urdf"),
+            glob(os.path.join("urdf", "*.xacro")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='rocotics',
-    maintainer_email='598062@stud.hvl.no',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="rocotics",
+    maintainer_email="598062@stud.hvl.no",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "tb3_blob_tracker = robot_vision.tb3_blob_tracker:main",
+            "command_sphere = robot_vision.command_sphere:main",
         ],
     },
 )
