003eb19f6f2ce3e2f498a99f5a99a9833c87fc5f
h598062 <598062@stud.hvl.no>
2024-09-30 22:47:05 +0200

    finish med wall follow

--- bug2_navigation/bug2_navigation/wallfollowercontroller.py
+++ bug2_navigation/bug2_navigation/wallfollowercontroller.py
@@ -60,6 +60,10 @@ class WallfollowerController(Node):
         lf = self.lidar_left_f
         lm = self.lidar_left_m
         lb = self.lidar_left_b
+
+        mode1_dst = 0.6
+        mode2_dst = 0.6
+        mode3_dst = 0.7
         # print(
         #     f"fl: {fl:.2f} - fm: {fm:.2f} - fr: {fr:.2f} - rf: {rf:.2f} - rm: {rm:.2f} - rb: {rb:.2f} - lf: {lf:.2f} - lm: {lm:.2f} - lb: {lb:.2f}"
         # )
@@ -68,7 +72,7 @@ class WallfollowerController(Node):
             # print(
             #     f"fl: {fl:.2f} - fm: {fm:.2f} - fr: {fr:.2f} - rf: {rf:.2f} - rm: {rm:.2f} - rb: {rb:.2f} - lf: {lf:.2f} - lm: {lm:.2f} - lb: {lb:.2f}"
             # )
-            dst = 0.6
+            dst = 0.5
             fw_speed = 0.2
             turn_speed = 0.0
             if fm < dst:
@@ -76,24 +80,35 @@ class WallfollowerController(Node):
                 self.tracking_wall = "right"
                 print("Found wall, activated mode 2: Turning")
         if self.mode == 2:
-            dst = 0.6
             fw_speed = 0.005
             if self.tracking_wall == "right":
-                print(f"rf: {rf:.2f} - rm: {rm:.2f} - rb: {rb:.2f}")
+                print(f"fm: {fm:.2f} - rf: {rf:.2f} - rm: {rm:.2f} - rb: {rb:.2f}")
                 turn_speed = 1.0
-                if rm <= rb and rm <= rf and abs(rb - rf) < 0.1 and rm < dst:
+                if (
+                    fm >= mode2_dst
+                    and rm <= rb
+                    and rm <= rf
+                    and abs(rb - rf) < 0.1
+                    and rm < mode2_dst
+                ):
                     turn_speed = 0.0
                     fw_speed = 0.0
                     self.mode = 1
-                    print("Activated mode 1")
+                    print("Finished turning, activated mode 1: follow wall")
             elif self.tracking_wall == "left":
-                print(f"lf: {lf:.2f} - lm: {lm:.2f} - lb: {lb:.2f}")
+                # print(f"lf: {lf:.2f} - lm: {lm:.2f} - lb: {lb:.2f}")
                 turn_speed = -1.0
-                if lm <= lb and lm <= lf and abs(lb - lf) < 0.1 and lm < dst:
+                if (
+                    fm >= mode2_dst
+                    and lm <= lb
+                    and lm <= lf
+                    and abs(lb - lf) < 0.1
+                    and lm < mode2_dst
+                ):
                     turn_speed = 0.0
                     fw_speed = 0.0
                     self.mode = 1
-                    print("Activated mode 1")
+                    print("Finished turning, activated mode 1: follow wall")
             else:
                 vel_msg.linear.x = 0.0
                 vel_msg.angular.z = 0.0
@@ -103,35 +118,46 @@ class WallfollowerController(Node):
                 )
 
         if self.mode == 1:
-            print(
-                f"fl: {fl:.2f} - fm: {fm:.2f} - fr: {fr:.2f} - rf: {rf:.2f} - rm: {rm:.2f} - rb: {rb:.2f} - lf: {lf:.2f} - lm: {lm:.2f} - lb: {lb:.2f}"
-            )
-            dst = 0.5
+            # print(
+            #     f"fl: {fl:.2f} - fm: {fm:.2f} - fr: {fr:.2f} - rf: {rf:.2f} - rm: {rm:.2f} - rb: {rb:.2f} - lf: {lf:.2f} - lm: {lm:.2f} - lb: {lb:.2f}"
+            # )
             fw_speed = 0.2
             if rm <= rb and rm <= rf:
                 turn_speed = 0.0
             else:
-                if rf > rm + dst:
+                if rf > rm + mode1_dst:
                     fw_speed = 0.05
                     if self.wall_dissappeared:
-                        if rm > rb:
+                        if rm > rb + mode1_dst:
                             print("Wall dissappeared, going mode 3")
                             self.mode = 3
-                            turn_speed = 0.0
                             fw_speed = 0.0
                     else:
                         print("wall disappeared maybe")
+                        self.wall_dissappeared = True
+                        turn_speed = 0.0
                 else:
                     turn_speed = constrain((rb - rf) * 2, -1.0, 1.0)
 
-            if fm < 0.6:
+            if fm < mode1_dst:
                 turn_speed = 0.0
                 fw_speed = 0.0
                 print("Found new wall, going mode 2")
                 self.mode = 2
 
         if self.mode == 3:
-            print("Mode 3 not implemented")
+            print(f"fm: {fm:.2f} - rf: {rf:.2f} - rm: {rm:.2f} - rb: {rb:.2f}")
+            turn_speed = -0.4
+            fw_speed = 0.2
+            if rf <= mode3_dst:
+                turn_speed = -0.1
+            if rm <= mode3_dst and rf <= mode3_dst + 0.2 and rb <= mode3_dst + 0.2:
+                print("re-found the wall, continuing mode 1")
+                fw_speed = 0.0
+                self.mode = 1
+            elif fm < mode2_dst:
+                print("found a wall in front, going mode 2")
+                self.mode = 2
 
         vel_msg.linear.x = fw_speed
         vel_msg.angular.z = turn_speed
