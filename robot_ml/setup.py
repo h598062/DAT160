11607e9bbf64929f76d49b8b900647efb281646a
h598062 <598062@stud.hvl.no>
2024-11-04 13:45:18 +0100

    done ros steps for assignment

--- robot_ml/setup.py
+++ robot_ml/setup.py
@@ -1,26 +1,48 @@
 from setuptools import setup
+import os
+from glob import glob
 
-package_name = 'robot_ml'
+package_name = "robot_ml"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*.launch.py")),
+        ),
+        (
+            os.path.join("share", package_name, "worlds"),
+            glob(os.path.join("worlds", "*.world")),
+        ),
+        (
+            os.path.join(
+                "share", package_name, "models", "painting", "materials", "scripts"
+            ),
+            glob("models/painting/materials/scripts/*"),
+        ),
+        (
+            os.path.join(
+                "share", package_name, "models", "painting", "materials", "textures"
+            ),
+            glob("models/painting/materials/textures/*"),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='rocotics',
-    maintainer_email='598062@stud.hvl.no',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="rocotics",
+    maintainer_email="598062@stud.hvl.no",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'robot_ml = robot_ml.robot_ml:main'
+        "console_scripts": [
+            "camera_viz = robot_ml.camera_viz:main",
+            "project_ml_idea = robot_ml.project_ml_idea:main",
         ],
     },
 )
